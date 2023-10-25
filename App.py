84f9be7f8af2f53ddd1bbdcfbd188f5fab3e04e5
Lanka Niranjan <lanka@bistecglobal.com>
2023-10-25 23:49:15 +0530

    Implement single resume forecast

--- App.py
+++ App.py
@@ -1,54 +1,46 @@
 import streamlit as st
 from dotenv import load_dotenv
-import DocumentHandler as PDFCharacterTextSplitter
-import EmbeddingHandler as PDFTextEmbedding
-import ConversationHandler as CurrentConversation
 import ResumeHandler as ResumeAnalasis
 from HTMLTemplate import css, CV_template, ANA_template
 
-def HandleUserQuestion(userQuestion):
-     questionResponse = st.session_state.conversation({'question':userQuestion})
-     st.write(questionResponse)
+#def HandleUserQuestion(userQuestion):
+#     questionResponse = st.session_state.conversation({'question':userQuestion})
+#     st.write(questionResponse)
 
 load_dotenv()
 st.set_page_config(page_title="CV Screening", page_icon=":document:")
 st.write(css, unsafe_allow_html=True)
 
-if "conversation" not in st.session_state:
-     st.session_state.conversation = None
+if "userQuestion" not in st.session_state:
+     st.session_state['userQuestion'] = ''
 
 with st.sidebar:
-     st.subheader("Available CV's")
-     pdf_docs = st.file_uploader("Upload your CVs here",accept_multiple_files=True)
+     st.subheader("Job Profile")
+     jobTitle = st.text_input("Job title:")
+     st.text_input("Experience level:")
+     st.text_input("Communication level:")
+     st.text_input("Company culture:")
+     st.subheader("Candidate Profile")
+     pdf_doc = st.file_uploader("Upload your candidate resume here and click 'Forecast'",accept_multiple_files=False)
      
-     #if st.button("Process"):
-     #     with st.spinner("Processing"):
-               
-               # Get CV text
-     #          rawPDFText = PDFCharacterTextSplitter.GetPDFText(pdf_docs)
-               #st.write(rawPDFText)
-               
-               # Get text chunks becasue we can't process tokensize exceed the limit (2048)
-     #          textChunks = PDFCharacterTextSplitter.GetTextChunks(rawPDFText)
-               #st.write(textChunks)
-
-               #Get text embedings
-     #          textEmbeddings = PDFTextEmbedding.GetVectoreStore(textChunks)
-               #st.write(textEmbeddings)
-
-               #Get conversation chain
-     #          st.session_state.conversation = CurrentConversation.GetConversationChain(textEmbeddings)
-
-st.header("Analyze the CV bank")
-userQuestion = st.text_input("Please enter your job title:")
-
-
-if st.button("Find") and userQuestion:
-      with st.spinner("Searching"):
-          resumeAnalysisData = ResumeAnalasis.GetResumeResult(pdf_docs,userQuestion)
-          #st.write(resumeAnalysisData)
-          for x,y in resumeAnalysisData.items():
-               st.write(CV_template.replace("{{MSG}}", x),unsafe_allow_html=True)
-               st.write(ANA_template.replace("{{MSG}}", y[0]["analysis"]),unsafe_allow_html=True)
-
+st.header("Analysis of candidate resume")
+if st.sidebar.button("Forecast"):
+     if jobTitle and pdf_doc:
+          st.session_state['userQuestion'] = "Please analyze this resume for  "+jobTitle+" post"
+          st.text_input("Forecast this candidate based on:",st.session_state['userQuestion'])   
+          with st.spinner("Analyzing"):
+               resumeAnalysisData = ResumeAnalasis.GetSingleResumeResult(pdf_doc,st.session_state['userQuestion'],jobTitle)
+               for x,y in resumeAnalysisData.items():
+                    jobDescription = st.text_area("Job description:",(y[0]["jobDescription"]).strip(),height=100)                    
+                    st.write(CV_template.replace("{{MSG}}", "Resume forecast:"),unsafe_allow_html=True)
+                    st.write(ANA_template.replace("{{MSG}}", (y[0]["analysis"]).strip()),unsafe_allow_html=True)
+                    st.sidebar.text_input("LinkedIn profile:",(y[0]["linkedinURL"]).strip())
+                    st.sidebar.text_input("FaceBook profile:",(y[0]["facebookURL"]).strip())
+                    st.sidebar.text_input("Github account:",(y[0]["githubURL"]).strip())
+                    st.sidebar.text_input("Hackerank:")
+                    st.sidebar.text_input("SO:")
+     else:
+          st.error('Please check whether "job title" inserted and "Resume" uploaded', icon="⚠️")                    
+              
+ 
 
--- ResumeHandler.py
+++ ResumeHandler.py
@@ -1,12 +1,90 @@
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader,PdfFileReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 
 # Analyze resumes
+#Single resume analysis
+
+def GetSingleResumeResult(pdfDoc, userQuestion,jobTitle):   
+     pdfDict = dict()
+     pdfReader = PdfReader(pdfDoc)
+     pdfName = pdfDoc.name
+     pdfText = ""
+     for pdfPage in pdfReader.pages:
+          pdfText += pdfPage.extract_text() 
+
+     texts = GetCVTextChunkAnalysis(pdfText)
+
+     pdfAnalysis = GetResumeAnalysis(userQuestion, texts)
+     linkedinURL = GetResumeAnalysis("what is the LinkedIn URL of this resume?", texts)
+     if "The LinkedIn URL of this resume is" in linkedinURL: 
+      linkedinURL = linkedinURL.replace("The LinkedIn URL of this resume is", "")     
+     if "This resume does not have a LinkedIn URL." in linkedinURL or "There is no LinkedIn URL in this resume." in linkedinURL: 
+      linkedinURL = ""
+     
+     githubURL = GetResumeAnalysis("what is the github URL of this resume?", texts)
+     if "The GitHub URL of this resume is" in githubURL: 
+      githubURL = githubURL.replace("The GitHub URL of this resume is", "") 
+     if "This resume does not have a GitHub URL." in githubURL or "There is no GitHub URL in this resume." in githubURL: 
+      githubURL = ""
+
+     facebookURL = GetResumeAnalysis("what is the FaceBook URL of this resume?", texts)
+     if "The Facebook URL of this resume is" in facebookURL: 
+      facebookURL = facebookURL.replace("The Facebook URL of this resume is", "") 
+     if "This resume does not have a Facebook URL." in facebookURL or "There is no Facebook URL in this resume." in facebookURL: 
+      facebookURL = ""
+
+     candidateEmail = GetResumeAnalysis("what is the candidate email of this resume?", texts).replace("The candidate email is ", "")
+     jobDescription = GetJobDescription(jobTitle)
+     pdfDict[pdfName] = [{ 'analysis': pdfAnalysis, 'jobDescription': jobDescription, 'linkedinURL': linkedinURL, 'githubURL': githubURL, 'facebookURL': facebookURL, 'candidateEmail': candidateEmail }]
+          
+     return pdfDict 
+
+def GetCVTextChunkAnalysis(cvContent):
+
+     text_splitter = CharacterTextSplitter(
+     separator="\n",
+     chunk_size= 800,
+     chunk_overlap=200,
+     length_function=len
+     )
+     texts = text_splitter.split_text(cvContent)
+     return texts
+
+def GetResumeAnalysis(userQuestion, texts):
+     cvAnalysis =""
+
+     embeddings = OpenAIEmbeddings()
+     document_search = FAISS.from_texts(texts,embeddings)
+     
+     chain = load_qa_chain(OpenAI(), chain_type="stuff")
+     query = "{jdTitle}".format(jdTitle = userQuestion)
+     docs = document_search.similarity_search(query)
+     cvAnalysis = chain.run(input_documents=docs,question=query)
+     return cvAnalysis
+
+def GetJobDescription(jobTitle):
+     cvAnalysis =""
+     llm = OpenAI(temperature=0.9)
+
+     prompt = PromptTemplate(
+     input_variables=["jobTitle"],
+     template="What is the description of {jobTitle} ?",
+     )
+     chain = LLMChain(llm=llm, prompt=prompt)
+     cvAnalysis = chain.run({"jobTitle":jobTitle})
+     return cvAnalysis
+
+#End single resume analysis
+
+#Multple resume analysis
+
 def GetResumeResult(pdfDocs, userQuestion):   
      pdfDict = dict()
      for pdfDoc in pdfDocs:
@@ -17,8 +95,8 @@ def GetResumeResult(pdfDocs, userQuestion):
                pdfText += pdfPage.extract_text() 
 
           pdfAnalysis = GetCVTextAnalysis(pdfText, userQuestion)
-          #pdfSentiment = testSent.GetSentimentScore(pdfAnalysis)
-          pdfDict[pdfName] = [{ 'analysis': pdfAnalysis, 'sentiment': '' }]
+          jobDescription = GetJobDescription('.NET developer')
+          pdfDict[pdfName] = [{ 'analysis': pdfAnalysis, 'JD': jobDescription }]
      
      return pdfDict  
 
@@ -43,4 +121,4 @@ def GetCVTextAnalysis(cvContent, userQuestion):
      cvAnalysis = chain.run(input_documents=docs,question=query)
      return cvAnalysis
 
-
+#End multiple resume analysis
